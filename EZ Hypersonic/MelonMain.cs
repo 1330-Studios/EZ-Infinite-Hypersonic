031a46c283c5a9d450220e92099a51c75031b3ff
Matt <2matthew05@gmail.com>
2021-12-28 08:17:51 -0600

    Initial Commit

---
+++ EZ Hypersonic/MelonMain.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+using Assets.Scripts.Models;
+using Assets.Scripts.Models.Towers.Behaviors.Attack;
+
+using HarmonyLib;
+
+using MelonLoader;
+
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+[assembly: MelonInfo(typeof(EZ_InfiniteHypersonic.MelonMain), "EZ Infinite Hypersonic", "1.0", "1330 Studios LLC")]
+[assembly: MelonColor(ConsoleColor.Red)]
+
+namespace EZ_InfiniteHypersonic;
+public sealed class MelonMain : MelonMod {
+
+    public override void OnApplicationStart() {
+        LoggerInstance.Msg("EZ Infinite Hypersonic Loaded!");
+
+        HarmonyInstance.Patch(AccessTools.Method(typeof(GameModelLoader), nameof(GameModelLoader.Load)), postfix: new(AccessTools.Method(typeof(MelonMain), nameof(MelonMain.OnGameLoad))));
+    }
+
+    public static void OnGameLoad(ref GameModel __result) {
+        __result.towers.AsParallel().ForAll(tower => {
+            tower.range = 999;
+            tower.behaviors.Do(model => {
+                AttackModel potentialAttack = model.TryCast<AttackModel>(); 
+                if (potentialAttack != null) {
+                    potentialAttack.range = 999;
+
+                    potentialAttack.weapons.Do(weapon => weapon.rate = 0);
+
+                    int length = potentialAttack.weapons.Length;
+                    for (int j = 0; j < length; j++)
+                        potentialAttack.weapons = potentialAttack.weapons.AddItem(potentialAttack.weapons[j]).ToArray();
+                }
+            });
+        });
+    }
+}
---
+++ EZ Hypersonic/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("EZ Infinite Hypersonic")]
+[assembly: AssemblyDescription("A simple, yet customizeable, hypersonic mod.")]
+[assembly: AssemblyCompany("1330 Studios LLC")]
+[assembly: AssemblyProduct("EZ Infinite Hypersonic")]
+[assembly: AssemblyCopyright("Copyright © 1330 Studios LLC 2021")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
